168931b05f0230d7469ee532692807786d87f750
Tiago de Almeida <tiago.engenheiro.petroleo@gmail.com>
2022-04-06 15:22:11 -0300

    updated website

--- GAMEMANIA/src/app/app-routing.module.ts
+++ GAMEMANIA/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { FooterComponent } from './views/footer/footer.component';
 import { HeaderComponent } from './views/header/header.component';
 import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
 
 const routes: Routes = [
   {path: "", component:HomeComponent},
   {path: "header", component:HeaderComponent},
-  {path: "footer", component:FooterComponent}
+  {path: "footer", component:FooterComponent},
+  {path: "login", component:LoginComponent}
 ];
 
 @NgModule({
